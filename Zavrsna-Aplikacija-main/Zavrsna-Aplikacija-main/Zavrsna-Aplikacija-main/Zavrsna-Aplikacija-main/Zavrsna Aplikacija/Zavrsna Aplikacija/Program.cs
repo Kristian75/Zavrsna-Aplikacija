1cb48b66b8015a8d3e15ed6d2b5e7ac8c7e7aa30
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2025-03-10 23:16:47 +0100

    Add files via upload

---
+++ Zavrsna-Aplikacija-main/Zavrsna-Aplikacija-main/Zavrsna-Aplikacija-main/Zavrsna-Aplikacija-main/Zavrsna Aplikacija/Zavrsna Aplikacija/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zavrsna_Aplikacija
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Prijava loginForma = new Prijava();
+            Application.Run(loginForma);
+
+            // Ako želiš da se glavna forma pokrene tek nakon uspješne prijave, napravi nešto slično ovome:
+            if (loginForma.DialogResult == DialogResult.OK)
+            {
+                Application.Run(new GlavnaForma(loginForma.Uloga));
+            }
+
+        }
+    }
+}
